509f82a7e610bb4a956c38f55003dc949e55678f
sdmahfuz <mahafuzul@algomatrix.co>
2021-06-19 21:15:28 +0600

    css changes

--- component/CustomHeader.js
+++ component/CustomHeader.js
@@ -11,7 +11,7 @@ import Image from "next/image";
 import HeaderItem from './HeadeItems'
 function Header() {
   return (
-    <header className="flex  flex-col sm:flex-row m-5 justify-between items-center h-auto ">
+    <header className="flex  flex-col sm:flex-row m-5  justify-between items-center h-auto ">
       <div className="flex flex-grow justify-evenly max-w-2xl">
           <HeaderItem title='Home' Icon={HomeIcon}/>
           <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
